04e2a39720daa8d62a4180939984bbf9e0028401
ouytang-dev <110030814+ouytang-dev@users.noreply.github.com>
2026-02-01 16:32:21 +1100

    Update control_flow.js

--- control_flow/control_flow.js
+++ control_flow/control_flow.js
@@ -1,3 +1,6 @@
+//
+// if else Exmaple
+//
 let userRole = "admin";
 let accessLevel;
 
@@ -10,3 +13,43 @@ if (userRole === "admin") {
 }
 
 console.log("Access Level:", accessLevel);
+
+//
+// Nested If Else  example
+//
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+//
+// Switch example
+//
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
